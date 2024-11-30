293fa05991b7b6eea55dd9edab2e75a469e0b69b
always7blue <cansu7sahin@gmail.com>
2024-11-30 16:55:32 +0300

    NavMesh added

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class EnemyAI : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float aggroRadius = 5f;
+    public float attackSpeed = 3f;
+    public Transform player;
+    public bool isAggroed = false;
+
+
     void Start()
     {
-        
+        player = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (isAggroed)
+        {
+            Vector3 direction = (player.position - transform.position).normalized;
+            transform.position += direction * attackSpeed * Time.deltaTime;
+            transform.LookAt(player);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isAggroed = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isAggroed = false;
+        }
     }
 }
