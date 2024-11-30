34e9873b26a42d0263f26ab64a0e2bbf05482cc6
always7blue <cansu7sahin@gmail.com>
2024-11-30 20:03:45 +0300

    zombie animation added

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -1,43 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI; // NavMeshAgent için gerekli
 
 public class EnemyAI : MonoBehaviour
 {
     public float aggroRadius = 5f;
-    public float attackSpeed = 3f;
     public Transform player;
+    private NavMeshAgent agent;
     public bool isAggroed = false;
-
 
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player").transform;
+        agent = GetComponent<NavMeshAgent>(); // NavMeshAgent bileþenine referans al
     }
 
     void Update()
     {
         if (isAggroed)
         {
-            Vector3 direction = (player.position - transform.position).normalized;
-            transform.position += direction * attackSpeed * Time.deltaTime;
-            transform.LookAt(player);
+            float distanceToPlayer = Vector3.Distance(transform.position, player.position);
+
+            if (distanceToPlayer > aggroRadius * 2) // Düþman çok uzaklaþtýðýnda takip býrakabilir
+            {
+                isAggroed = false;
+                agent.ResetPath();
+            }
+            else
+            {
+                agent.SetDestination(player.position);
+            }
         }
     }
+
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            isAggroed = true;
-        }
-    }
-
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.CompareTag("Player"))
-        {
-            isAggroed = false;
+            isAggroed = true; // Oyuncu algýlandý, takip baþlasýn
         }
     }
 }
